347521c79db81677e8049ecbdc1716b84873a5cf
pistatium <kimihiro.nisimura@gmail.com>
2013-11-23 19:45:16 +0900

    modify templete

--- server_appengine/syskey.py
+++ server_appengine/syskey.py
@@ -4,6 +4,4 @@
 # this file does not expose to public
 #
 
-device_key_pre  = "Z4=26*gWr:6&aw3gR1fVeq"
-device_key_post = "rw3Hv=4Hw*64;rw455G-r"
-token_key       = "#gT9_aQp@s@D3AveDe2z1="
+token_key       = "#gT9_aQp@s@EZAveDe2z="
