b0d3e93c593affb71bd4cdda24641c596383f360
kimihiro n <kimihiro.nisimura@gmail.com>
2015-07-29 09:58:05 +0900

    Setup test dir

--- server_appengine/tests/__init__.py
+++ server_appengine/tests/__init__.py
@@ -2,3 +2,13 @@
 
 from __future__ import absolute_import, division, print_function
 
+from django.conf import settings
+
+settings.configure(
+    DATABASES={
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
+    }
+)
---
+++ server_appengine/tests/test_urls.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+from __future__ import absolute_import, division, print_function
+
+from django.test import TestCase
+
+
+class UrlRoutingTest(TestCase):
+    def test_dummy(self):
+        """ Always success test for setup tests """
+        self.assertTrue(True)
