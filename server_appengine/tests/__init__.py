1996b923053073bfd814c5eeea2bc4d8ef25471d
kimihiro n <kimihiro.nisimura@gmail.com>
2015-07-31 21:53:48 +0900

    Modify test setup

--- server_appengine/tests/__init__.py
+++ server_appengine/tests/__init__.py
@@ -2,13 +2,28 @@
 
 from __future__ import absolute_import, division, print_function
 
-from django.conf import settings
+import django.conf
 
-settings.configure(
-    DATABASES={
+from .. import settings
+
+'''
+  load setting file
+  http://stackoverflow.com/questions/5122414/django-googleappengine-error-django-settings-module-is-undefined
+'''
+# Filter out all the non-setting attributes of the settings module
+settingsKeys = filter(lambda name: str(name) == str(name).upper(), 
+                      dir(settings))
+
+# copy all the setting values from the settings module into a dictionary
+settingsDict = dict(map(lambda name: (name, getattr(settings, name)), 
+                        settingsKeys))
+
+settingsDict.update({
+    "DATABASES": {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": ":memory:",
         }
     }
-)
+})
+django.conf.settings.configure(**settingsDict)
